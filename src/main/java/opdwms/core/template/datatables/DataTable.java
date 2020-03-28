80bd940f7431fe8c75eaba7ac28b8b4387b873e7
neshoj <ignatiusojiambo@gmail.com>
2020-03-29 01:16:03 +0300

    v0.0.48
    Bug fix:
    Corrected value for number of filtered fields

--- src/main/java/opdwms/core/template/datatables/DataTable.java
+++ src/main/java/opdwms/core/template/datatables/DataTable.java
@@ -396,11 +396,11 @@ public class DataTable implements DatatablesInterface {
 //        map.put("iTotalRecords", buildResultSet("total").iterator().next());
 //        map.put("iTotalDisplayRecords", buildResultSet("filtered-total").iterator().next());
 
-        Object[] totals = buildResultSet("total").iterator().next();
-        map.put("iTotalRecords", totals.length);
-        map.put("iTotalDisplayRecords", totals.length);
+        List<Object[]> total = buildResultSet("total");
+        map.put("iTotalRecords", total.iterator().next());
+        map.put("iTotalDisplayRecords",  total.iterator().next());
         if (!_footerColumns.isEmpty()) {
-            map.put("footerTotals", totals.length);
+            map.put("footerTotals",  total.iterator().next());
 //            map.put("footerTotals", buildResultSet("footer-totals"));
         }
 
