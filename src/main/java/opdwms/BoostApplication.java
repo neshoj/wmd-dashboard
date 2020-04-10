b7a06dc4e239a58864c61ac29e228b18b2429517
nthusi <pnthusi@one-point-seven.com>
2020-04-10 22:08:22 +0300

    Add generate vehicle overloaded vhm scheduled report gen

--- src/main/java/opdwms/BoostApplication.java
+++ src/main/java/opdwms/BoostApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 
@@ -11,6 +12,7 @@ import java.util.concurrent.Executor;
 
 @SpringBootApplication
 @EnableWebSocketMessageBroker
+@EnableScheduling
 @EnableAsync
 public class BoostApplication {
 
---
+++ src/main/java/opdwms/web/reports/services/ReportService.java
@@ -0,0 +1,144 @@
+package opdwms.web.reports.services;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+public class ReportService {
+
+    private final JdbcTemplate jdbcTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(ReportService.class);
+    public static final String GVM_OVERLOAD = "2";
+
+
+    @Autowired
+    public ReportService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Scheduled(cron = "${app.daily.cron.expression}")
+    public void scheduledGVMReportTask() {
+        logger.info("starting cron job");
+//        Date date = new Date();
+//        String sampleDate = "2019-02-22";
+//        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+//        Date date;
+//        try {
+//            date = formatter.parse(sampleDate);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//            return;
+//        }
+
+        Calendar calendar = Calendar.getInstance();
+//        calendar.setTime(date);
+        calendar.add(Calendar.DATE, -1);
+
+        Date yesterday = calendar.getTime();
+        generateVehiclesOverloadedOnGVMReport(yesterday);
+    }
+
+
+    private void generateVehiclesOverloadedOnGVMReport(Date date) {
+
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            String reportDate = formatter.format(date);
+            //get station names first
+            String getStationsQuery = "SELECT name FROM weighbridge_stations";
+
+//        String getStationsQuery = "SELECT  DISTINCT ws.name FROM weighing_transactions WT LEFT JOIN weighbridge_stations ws ON WT.weighbridge_no = ws.id WHERE cast(wt.transaction_date AS DATE) = '2019-10-30'";
+            List<String> list = jdbcTemplate.queryForList(getStationsQuery)
+                    .stream()
+                    .map(w -> (String) w.entrySet().iterator().next().getValue())
+                    .collect(Collectors.toList());
+
+            StringBuilder finalQueryBuilder = new StringBuilder();
+
+            for (String station : list) {
+                String stationSubQuery = "coalesce((SELECT count(ws.name) FROM weighbridge_stations ws2 where ws.id=ws2.id and ws.name='" + station + "'),0) AS '" + station + "'";
+                finalQueryBuilder.append(",")
+                        .append(stationSubQuery);
+            }
+
+            String query = "SELECT wt.axle_configuration" + finalQueryBuilder.toString() +
+                    " FROM weighing_transactions wt" +
+                    " LEFT JOIN weighbridge_stations ws ON wt.weighbridge_no = ws.id" +
+                    " WHERE cast(wt.transaction_date AS DATE) = '" + reportDate + "' AND wt.status='"+GVM_OVERLOAD+"' GROUP BY wt.axle_configuration";
+
+            List<Map<String, Object>> result = jdbcTemplate.queryForList(query);
+
+            JSONArray array = new JSONArray();
+            for (Map<String, Object> row : result) {
+                JSONObject object = new JSONObject();
+                for (Map.Entry<String, Object> entry : row.entrySet()) {
+
+                    if (entry.getKey().equals("axle_configuration")) {
+                        object.put("AXLE CONFIG", entry.getValue());
+                    } else {
+                        object.put((entry.getKey()), entry.getValue());
+                        array.put(object);
+                    }
+                }
+            }
+            logger.debug("Generated report. Json value: {}", array.toString());
+            logger.debug("Generated report. Map value: {}", StringUtils.join(result));
+
+            //update report or create new record
+
+            //check if previous record exists
+            String checkReportSql = "SELECT * FROM vehicles_overloaded_vhm_daily_reports WHERE date='"+reportDate+"' ";
+            List<Map<String,Object>> checkReportRes =  jdbcTemplate.queryForList(checkReportSql);
+
+            if(checkReportRes.isEmpty()){
+                logger.debug("No existing report found, creating new one");
+                String insertionSql = "INSERT INTO vehicles_overloaded_vhm_daily_reports(date,report_json,report_map) VALUES (?,?,?)";
+                Date finalReportDate = formatter.parse(reportDate);
+                System.out.println("report date: " + finalReportDate);
+                KeyHolder holder = new GeneratedKeyHolder();
+                jdbcTemplate.update(connection -> {
+                    PreparedStatement ps = connection.prepareStatement(insertionSql, Statement.RETURN_GENERATED_KEYS);
+                    ps.setDate(1, new java.sql.Date(finalReportDate.getTime()));
+                    ps.setString(2, array.toString());
+                    ps.setString(3, StringUtils.join(result));
+                    return ps;
+                }, holder);
+            }else {
+                logger.debug("Found existing reports of size {}", checkReportRes.size() );
+                Integer id = (int)checkReportRes.get(0).get("id");
+                String updateReportSql = "UPDATE vehicles_overloaded_vhm_daily_reports SET report_json=?,report_map=? WHERE id=? ";
+
+                jdbcTemplate.update(connection -> {
+                    PreparedStatement ps = connection.prepareStatement(updateReportSql, Statement.NO_GENERATED_KEYS);
+                    ps.setString(1, array.toString());
+                    ps.setString(2, StringUtils.join(result));
+                    ps.setInt(3, id);
+                    return ps;
+                });
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
