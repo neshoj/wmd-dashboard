d7195d2f75e9edd8a46b1498edf23dae64e467c7
Ignatius Ojiambo <iojiambo@payconnect.co.ke>
2020-07-25 11:39:21 +0300

    v0.0.73
    Bug Fix
    1.Fixed date conversion from GMT to EAT

--- src/main/java/opdwms/core/template/datatables/DataTable.java
+++ src/main/java/opdwms/core/template/datatables/DataTable.java
@@ -149,13 +149,10 @@ public class DataTable implements DatatablesInterface {
           // TODO use regex later on
           if (Arrays.asList(_esDateFields).contains(_esDocFields[x])) {
             String source = (String) sourceAsMap.get(_esDocFields[x]);
-            System.out.println("=====================================source = " + source);
 
             Date date = parser.parse(source);
-            System.out.println("=====================================date = " + date);
             String formattedDate = formatter.format(date);
             docValues[x] = formattedDate;
-            System.out.println("=====================================formattedDate = " + formattedDate);
           } else {
             docValues[x] = sourceAsMap.get(_esDocFields[x]);
           }
